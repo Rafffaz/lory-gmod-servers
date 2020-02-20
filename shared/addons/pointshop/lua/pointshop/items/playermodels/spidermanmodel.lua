78a563f6f8cc260b1b4a32723225bed69a8aacf2
Rafffaz <61294031+Rafffaz@users.noreply.github.com>
2020-02-20 17:35:28 -0300

    Raffaz__ Adicionou Spider Man Model

---
+++ shared/addons/pointshop/lua/pointshop/items/playermodels/spidermanmodel.lua
@@ -0,0 +1,4 @@
+https://steamcommunity.com/sharedfiles/filedetails/?id=1154384700&searchtext=playermodel
+ITEM.Name = "Spider Man Civil War"
+ITEM.Price = 10000
+ITEM.Model = "models/kryptonite/spiderman_2/spiderman_2.mdl"
